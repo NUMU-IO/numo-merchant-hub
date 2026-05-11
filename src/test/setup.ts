2b525d73260625b65d31ad9efdcc6d87a309083c
Yahiasherif002 <yahyasheriif@gmail.com>
2026-05-11 21:02:28 +0300

    fix

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,7 @@
 import "@testing-library/jest-dom";
+import { vi } from "vitest";
+
+vi.stubEnv("VITE_API_URL", "http://localhost/api/v1");
 
 Object.defineProperty(window, "matchMedia", {
   writable: true,
